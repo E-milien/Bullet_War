952d0f1a3964ae53d6a235c2dd937b133eb11fce
Émilien <emilien.roukine@etu.univ-smb.fr>
2023-01-05 11:09:53 +0100

    ajout scene de mort

---
+++ solution/SAE_DEV_PROJ/DeadScreen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Screens;
+
+namespace SAE_DEV_PROJ
+{
+    public class DeadScreen : GameScreen
+    {
+        private SpriteBatch _spriteBatch;
+        private Game1 _myGame;
+        private SpriteFont _police;
+        // pour récupérer une référence à l’objet game pour avoir accès à tout ce qui est
+        // défini dans Game1
+        public DeadScreen(Game1 game) : base(game)
+        {
+            _police = Content.Load<SpriteFont>("Font");
+            _myGame = game;
+        }
+        public override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+        public override void Draw(GameTime gameTime)
+        {
+
+            _myGame.GraphicsDevice.Clear(Color.Orange); // on utilise la reference vers Game1 pour chnager le graphisme
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(_police, "You're dead", new Vector2(20, 20), Color.White);
+            _spriteBatch.DrawString(_police, "Press ENTER to start", new Vector2(500, 500), Color.Black);
+
+            _spriteBatch.End();
+        }
+    }
+}
+
--- solution/SAE_DEV_PROJ/Game1.cs
+++ solution/SAE_DEV_PROJ/Game1.cs
@@ -13,6 +13,7 @@ namespace SAE_DEV_PROJ
         private GraphicsDeviceManager _graphics;
         HomeScreen _homeScreen;
         PlayScreen _playScreen;
+        DeadScreen _deadScreen;
         private bool _loaded;
         
         
@@ -40,7 +41,7 @@ namespace SAE_DEV_PROJ
         {
             _homeScreen = new HomeScreen(this); // en leur donnant une référence au Game
             _playScreen = new PlayScreen(this);
-
+            _deadScreen = new DeadScreen(this);
             // TODO: use this.Content to load your game content here
         }
 
@@ -57,10 +58,11 @@ namespace SAE_DEV_PROJ
             {
                 _screenManager.LoadScreen(_homeScreen, new FadeTransition(GraphicsDevice, Color.Black));
             }
-            else if (keyboardState.IsKeyDown(Keys.Enter))
+            if (keyboardState.IsKeyDown(Keys.Enter))
             {
                 _screenManager.LoadScreen(_playScreen, new FadeTransition(GraphicsDevice, Color.Black));
             }
+
 
             base.Update(gameTime);
         }
