c620630c7929c023aa09097350490cb7c74d6f4b
Émilien <emilien.roukine@etu.univ-smb.fr>
2023-01-05 11:19:51 +0100

    ajout winScreen

--- solution/SAE_DEV_PROJ/WinScreen.cs
+++ solution/SAE_DEV_PROJ/WinScreen.cs
@@ -10,14 +10,14 @@ using MonoGame.Extended.Screens;
 
 namespace SAE_DEV_PROJ
 {
-    public class HomeScreen : GameScreen
+    public class WinScreen : GameScreen
     {
         private SpriteBatch _spriteBatch;
         private Game1 _myGame;
         private SpriteFont _police;
         // pour récupérer une référence à l’objet game pour avoir accès à tout ce qui est
         // défini dans Game1
-        public HomeScreen(Game1 game) : base(game)
+        public WinScreen(Game1 game) : base(game)
         {
             _police = Content.Load<SpriteFont>("Font");
             _myGame = game;
@@ -29,20 +29,21 @@ namespace SAE_DEV_PROJ
         }
 
         public override void Update(GameTime gameTime)
-        { 
+        {
 
         }
         public override void Draw(GameTime gameTime)
         {
 
-            _myGame.GraphicsDevice.Clear(Color.MediumVioletRed); // on utilise la reference vers Game1 pour chnager le graphisme
+            _myGame.GraphicsDevice.Clear(Color.Orange); // on utilise la reference vers Game1 pour chnager le graphisme
             _spriteBatch.Begin();
-            _spriteBatch.DrawString(_police, "Bullets WAR", new Vector2(20, 20), Color.White);
-            _spriteBatch.DrawString(_police, "Press ENTER to start", new Vector2(500,500), Color.Black);
+            _spriteBatch.DrawString(_police, "You won !", new Vector2(20, 20), Color.White);
+            _spriteBatch.DrawString(_police, "Press ENTER to play again", new Vector2(500, 500), Color.Black);
             _spriteBatch.DrawString(_police, "Press Q to quit", new Vector2(500, 530), Color.Black);
-
             _spriteBatch.End();
         }
     }
 }
+
+
 
