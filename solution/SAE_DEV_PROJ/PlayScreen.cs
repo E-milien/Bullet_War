307857f6aebd5bb89726d6b49d0c2f4ad25b610c
Émilien <emilien.roukine@etu.univ-smb.fr>
2023-01-05 18:06:18 +0100

    version final du 05/01 stable

--- solution/SAE_DEV_PROJ/PlayScreen.cs
+++ solution/SAE_DEV_PROJ/PlayScreen.cs
@@ -29,8 +29,11 @@ namespace SAE_DEV_PROJ
         private bool _redemption;
         private double _chrono;
         private int _i;
-        private int var;
-        private int var2;
+        private int _i2;
+        private int _var;
+        private double _varCercle;
+        private bool _ok1;
+        private int _var2;
         private float _angle = 0f;
         private double _pvDepart;
         private Vector2 _positionPv = new Vector2(20, 30);
@@ -68,19 +71,24 @@ namespace SAE_DEV_PROJ
 
         public override void Initialize()
         {
+            // initialisation toutes les veriables
+            _ok1 = false;
             _bossAlive = true;
             _alive = true;
-            var = 0;
+            _var = 0;
             _i = -1;
+            _i2 = 0;
             _chrono = 0;
-            var2 = 2;
+            _var2 = 2;
+            _varCercle = 0;
+            _angle = 0f;
 
+            // initialisation boss & perso
             boss1 = new Boss(5000, 20, "boss", new Vector2(Constantes._LARGEUR_FENETRE / 2, Constantes._HAUTEUR_FENETRE / 5) - new Vector2(Constantes._LARGEUR_BOSS / 2, 0));
             hero = new Perso(false, 100, 5, 0, "perso", 1, 500, new Vector2(500, 500) - new Vector2(Constantes._LARGEUR_PERSO / 2, 0));
 
             _damagePerso = hero.DamagePerso;
             _pvDepart = hero.PvPerso;
-            // Bullets initialize
 
             // Bullets initialize
             for (int i = 0; i < _tabBullets.GetLength(0); i++)
@@ -120,33 +128,30 @@ namespace SAE_DEV_PROJ
                 _angle += 10f;
             }
 
-            _angle = 0f;
             // Initialize timer spirale
             _timer = new System.Timers.Timer(100);
             _timer.AutoReset = true;
             _timer.Enabled = true;
-
-
-            _police = Content.Load<SpriteFont>("Font");
 
             base.Initialize();
         }
 
         public override void LoadContent()
         {
-            
+            _police = Content.Load<SpriteFont>("Font");
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             _texturePerso = Content.Load<Texture2D>("perso");
             _textureBullet = Content.Load<Texture2D>(_tabBullets[0,0].Skin);
             _textureBoss = Content.Load<Texture2D>(boss1.SkinBoss);
 
+            // barre de vie perso
             _texture_Full = Content.Load<Texture2D>("Full");
             _texture_High = Content.Load<Texture2D>("High");
             _texture_Mid = Content.Load<Texture2D>("Mid");
             _texture_Low = Content.Load<Texture2D>("Low");
             _texture_VeryLow = Content.Load<Texture2D>("VeryLow");
             _texture_Dead = Content.Load<Texture2D>("Dead");
-            // TODO: use this.Content to load your game content here
+
             base.LoadContent();
         }
 
@@ -158,19 +163,13 @@ namespace SAE_DEV_PROJ
             float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
             //pattern1 pour les différentes "vague de bullets"
             _chrono += deltaTime;
-            if (_chrono >= var && _i<_tabBullets.GetLength(0)-1)
-            {
-                var += 2;
-                _i++;
-              
-            }
-            if (_chrono<25)
-                Pattern1(deltaTime, _i, _chrono);
-            if(_chrono >= var2)
+
+            //perd du score a cause du temps
+            if(_chrono >= _var2)
             {
                 if(hero.Score >= 100)
                     hero.Score -= 100;
-                var2 += 3;
+                _var2 += 3;
             }
 
             //tirs alliés
@@ -184,13 +183,21 @@ namespace SAE_DEV_PROJ
                 _tabBulletPerso[i].BulletPosition -= new Vector2(0, _tabBulletPerso[i].Vitesse * deltaTime);
             }
             Redemption(deltaTime);
-
+            // active le 1er partterne (pattern1)
+            if (_chrono< 19)
+               Pattern1(deltaTime, _i, _chrono);
             //lancer pattern2 au bout de 24 sec
-            if(_chrono>=22)
-                Pattern2(deltaTime);
-            if(_chrono>=35)
+            if(_chrono>=17&&_chrono<=25)
+               Pattern2(deltaTime);
+            //active le 3eme patterne (paterncercle)
+            if (_chrono > 22 && _chrono < 42)
+            {
                 PatternCercle(_angle);
-            throw new Exception("j'ai pas fini de codé ca donc je lance ca avant bug, je dois faire comme pour le paterne1 c'est a dire lancer le truc toutes les 2 secondes");
+                if (!_ok1)
+                    _varCercle = _chrono;
+                    _ok1 = true;
+            }
+
             DeplacementPerso(deltaTime);
             CollisionBoss();
             
@@ -210,9 +217,28 @@ namespace SAE_DEV_PROJ
             _spriteBatch.DrawString(_police, $"Vie Boss : { boss1.BossHP}", _positionPvBoss, Color.White);
             _spriteBatch.DrawString(_police, $"Score : {hero.Score}", new Vector2(_positionScore.X, _positionScore.Y - 50), Color.White);
 
-            
+            //HP
+            if (Math.Round((hero.PvPerso / _pvDepart) * 100) > 80)
+                _spriteBatch.Draw(_texture_Full, _positionPv, Color.White);
 
-            //Bullets adverses
+            else if (Math.Round((hero.PvPerso / _pvDepart) * 100) > 60)
+                _spriteBatch.Draw(_texture_High, _positionPv, Color.White);
+
+            else if (Math.Round((hero.PvPerso / _pvDepart) * 100) > 40)
+                _spriteBatch.Draw(_texture_Mid, _positionPv, Color.White);
+
+            else if (Math.Round((hero.PvPerso / _pvDepart) * 100) > 20)
+                _spriteBatch.Draw(_texture_Low, _positionPv, Color.White);
+
+            else if (Math.Round((hero.PvPerso / _pvDepart) * 100) > 0)
+                _spriteBatch.Draw(_texture_VeryLow, _positionPv, Color.White);
+
+            else
+                _spriteBatch.Draw(_texture_Dead, _positionPv, Color.White);
+
+            _spriteBatch.DrawString(_police, $"{hero.PvPerso} / {_pvDepart}", new Vector2(_positionPv.X * 10, _positionPv.Y + 10), Color.Black);
+
+            //Bullets pattern 1
             for (int z = 0; z <= _i; z++)
             {
                 for (int j = 0; j < _tabBullets.GetLength(1); j++)
@@ -236,31 +262,9 @@ namespace SAE_DEV_PROJ
             {
                 _spriteBatch.Draw(_textureBullet, _tabBullets2[i].BulletPosition - new Vector2(Constantes._LARGEUR_BULLETS / 2, 0), Color.Black);
             }
-
-            //HP
-            
-            if (Math.Round((hero.PvPerso / _pvDepart) * 100) > 80)
-                _spriteBatch.Draw(_texture_Full, _positionPv, Color.White);
-
-            else if (Math.Round((hero.PvPerso / _pvDepart) * 100) > 60)
-                _spriteBatch.Draw(_texture_High, _positionPv, Color.White);
-
-            else if (Math.Round((hero.PvPerso / _pvDepart) * 100) > 40)
-                _spriteBatch.Draw(_texture_Mid, _positionPv, Color.White);
-
-            else if (Math.Round((hero.PvPerso / _pvDepart) * 100) > 20)
-                _spriteBatch.Draw(_texture_Low, _positionPv, Color.White);
-
-            else if (Math.Round((hero.PvPerso / _pvDepart) * 100) > 0)
-                _spriteBatch.Draw(_texture_VeryLow, _positionPv, Color.White);
-
-            else
-                _spriteBatch.Draw(_texture_Dead, _positionPv, Color.White);
-
-            _spriteBatch.DrawString(_police, $"{hero.PvPerso} / {_pvDepart}", new Vector2(_positionPv.X * 10, _positionPv.Y + 10), Color.Black);
             
             //Bullets patternCercle
-            for (int i = 0; i < _tabBulletsCercle.GetLength(0); i++)
+            for (int i = 0; i <= _i2; i++)
             {
                 for (int j = 0; j < _tabBulletsCercle.GetLength(1); j++)
                 {
@@ -304,7 +308,6 @@ namespace SAE_DEV_PROJ
         internal bool Collision(bool ok, Bullet[,] _tableau)
         {
             bool tmp = false;
-
             if (ok == true)
                 return false;
 
@@ -325,7 +328,6 @@ namespace SAE_DEV_PROJ
         internal bool Collision(bool ok, Bullet[] _tableau)
         {
             bool tmp = false;
-
             if (ok == true)
                 return false;
 
@@ -336,9 +338,7 @@ namespace SAE_DEV_PROJ
                 if (rect1.Intersects(rect2))
                 {
                     tmp = true;
-                    
                 }
-            
             }
             return tmp;
         }
@@ -360,10 +360,9 @@ namespace SAE_DEV_PROJ
             }
         }
 
-
+        // Une fois arrivée en bas , les bullets sont remises en-dessous de la fenêtre
         public void BulletAllieReset()
         {
-        // Une fois arrivée en bas , les bullets sont remises en-dessous de la fenêtre
             for (int i = 0; i<_tabBulletPerso.Length; i++)
             {
                 if (_tabBulletPerso[i].BulletPosition.Y <= 0)
@@ -372,8 +371,16 @@ namespace SAE_DEV_PROJ
                 }
             }
         }
+
+        // 1
         public void Pattern1(float deltaTime, int i, double chrono)
         {
+            if (chrono >= _var && _i < _tabBullets.GetLength(0))
+            {
+                _var += 2;
+                _i++;
+            }
+
             Random rdn = new Random();
             if (chrono < 24)
             {
@@ -399,6 +406,7 @@ namespace SAE_DEV_PROJ
             }
         }
 
+        // 2
         public void Pattern2(float deltaTime)
         {
             for (int i = 0; i < _tabBullets2.Length; i++)
@@ -416,26 +424,45 @@ namespace SAE_DEV_PROJ
             }
         }
 
-        //Ca c'est la galère ptdr aled
+        // 3
         public void PatternCercle(float angle)
         {
-
-            for (int i = 0; i < _tabBulletsCercle.GetLength(0); i++)
+            if (_chrono >= _varCercle&& _i2 < _tabBulletsCercle.GetLength(1))
             {
-                for (int j = 0; i < _tabBulletsCercle.GetLength(1); i++)
+                _varCercle += 3;
+                _i2++;
+            }
+            if (_chrono < 42)
+            {
+                for (int z = 0; z < _i2; z++)
                 {
-                    float bulletDirectionX = MathF.Sin(angle * MathF.PI / 180f);
-                    float bulletDirectionY = MathF.Cos(angle * MathF.PI / 180f);
-                    Vector2 bulletDirection = new Vector2(bulletDirectionX, bulletDirectionY);
+                    for (int j = 0; j < _tabBulletsCercle.GetLength(1); j++)
+                    {
+                        float bulletDirectionX = MathF.Sin(angle * MathF.PI / 180f);
+                        float bulletDirectionY = MathF.Cos(angle * MathF.PI / 180f);
+                        Vector2 bulletDirection = new Vector2(bulletDirectionX, bulletDirectionY);
 
-                    _tabBulletsCercle[i,j].BulletPosition += bulletDirection;
+                        _tabBulletsCercle[z, j].BulletPosition += bulletDirection;
 
-                    angle += 10f;
+                        angle += 10f;
+                    }
+
                 }
+            }
+            else
+            {
+                for (int z = 0; z < _i2; z++)
+                {
+                    for (int j = 0; j < _tabBulletsCercle.GetLength(1); j++)
+                    {
+                        _tabBulletsCercle[z, j].BulletPosition = new Vector2(-20, -20);
+                    }
+                }
+
             }
         }
 
-        //galère suite AAAAAAAAAAAAAAAAAAAAAAA(pattern spirale)
+        // 4
         public void PatternSpirale(float angle)
         {
             for (int i = 0; i < _tabBulletsSpirale.Length; i++)
@@ -448,10 +475,9 @@ namespace SAE_DEV_PROJ
             
                 angle += 10f;
             }
-            
         }
 
-
+        // redemption de 2 secondes après être touché
         public void Redemption(float deltaTime)
         {
             if (Collision(_redemption, _tabBullets2)||Collision(_redemption, _tabBullets) && _redemption == false)
@@ -476,6 +502,8 @@ namespace SAE_DEV_PROJ
                 _alive = false;
             }
         }
+
+        // si le boss est mort
         internal void CheckBossDead(Boss boss)
         {
             if (boss.BossHP<=0)
@@ -484,6 +512,8 @@ namespace SAE_DEV_PROJ
                 boss.BossHP = 0;
             }
         }
+
+
     }
 }
 
